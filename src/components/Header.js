8303add5d9d937d392cdbad5472961b8713e1aa2
vrushtikaria <20it052@charusat.edu.in>
2022-06-02 15:15:46 +0530

    my first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui  menu">
+      <div className="ui container center">
+      <hr />
+        <h2>Contact Manager</h2>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
